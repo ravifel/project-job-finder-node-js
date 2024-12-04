8c5de8f7d3427f97ede644d50c15b55dbd0edd65
Ravi Silva <ravifelipee@gmail.com>
2024-12-04 06:44:07 -0300

    Implementation of a job details page

--- routes/jobs.js
+++ routes/jobs.js
@@ -3,9 +3,19 @@ const router = express.Router();
 const Job = require('../models/Job');
 
 // Route test
-router.get('/test', (require, response) => {
+router.get('/test', (request, response) => {
     response.send("Testing...");
 });
+
+// Job Details
+router.get('/view/:id', (request, response) => Job.findOne({
+    where: { id: request.params.id } // Here the request and the parameters that come with it are used.
+}).then(job => { // Encapsulating the vacancy in the method.
+    response.render('view', {
+        job // Rendered job
+    });
+}).catch(err => console.log(err))
+)
 
 // Search for Jobs
 router.get('/add', (request, response) => {
